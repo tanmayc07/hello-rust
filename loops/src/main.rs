edc20fb6c09692b10f9fb70f3b2aa96ae4a87777
Tanmay Chaudhari <hctanmay@gmail.com>
2024-01-22 22:02:54 -0500

    :sparkles: Update loops

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -40,5 +40,26 @@ fn main() {
     println!("End count = {count}");
 
     // conditional loops with while
-    
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    // for loop
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    // for loop using range
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
 }
