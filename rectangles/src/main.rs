ff0e56b23eac467f7a07d9f2e989ff3a3446ad85
Tanmay Chaudhari <hctanmay@gmail.com>
2024-01-25 17:07:57 -0500

    :sparkles: Update rectangles with impl

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -10,6 +10,19 @@ impl Rectangle {
     }
 }
 
+impl Rectangle{
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let _scale = 2;
     let rect1 = Rectangle {
@@ -17,7 +30,24 @@ fn main() {
         height: 50,
     };
 
-    println!("The area of rectangle is {} square pixels.", rect1.area());
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    let square1 = Rectangle::square(3);
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect2 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("Square = {square1:?}");
+    println!("area of square = {}", square1.area());
+
+    // println!("The area of rectangle is {} square pixels.", rect1.area());
     // println!("The rectange is {:?}", rect1);
     // dbg!(&rect1);
 }
